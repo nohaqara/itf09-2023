0fd80882773adfc24457a168931897a249c8e058
LTC2023 <nanaalqara@gmail.com>
2023-09-17 18:53:03 +0300

    Second Commit

---
+++ test.py
@@ -0,0 +1,68 @@
+#def add(x,y):
+   # total = x + y
+   # return total
+    #print(total)
+
+
+#num1 =int (input("entar num 1"))
+#num2 =int (input("entar num 2"))
+#print (add(num1,num2))
+
+
+
+#def sub(n,m):
+   # tot=n-m
+   # return tot
+
+#num3=int(input("entar num3"))
+#num4=int(input("entar num4"))
+#print(sub(num3,num4))
+
+
+#x=3
+#y=4
+#num = x+y
+#print(num)
+
+
+
+#x,y =5,4
+#num= x-y
+#print(num)
+
+
+
+
+
+
+
+#def div(a,b,c):
+  #  di=a/b/c
+   # return di
+
+
+#num5=int(input("entar num5"))
+#num6=int(input("entar num6"))
+#num7=int(input("entar num7"))
+#print(div(num5,num6,num7))
+
+
+
+def mul(x,y):
+    mu=x*y
+    return mu
+
+num1=int(input("entar num1"))
+num2=int(input("entar num1"))
+print(mul(num1,num2))
+
+
+
+
+
+
+
+
+
+
+
